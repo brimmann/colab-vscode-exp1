9e47100467ba6a671f033750c6e5afae29f7fe44
Rashid <rashidstanikzai.net@gmail.com>
2025-01-14 17:04:40 +0500

    nothing

--- dataset_prep.py
+++ dataset_prep.py
@@ -2,10 +2,12 @@ import tensorflow as tf
 from tensorflow.keras.datasets import mnist
 import matplotlib.pyplot as plt
 
+from types import SimpleNamespace
+
 # Load the dataset
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
-dataset = {train_images, test_images, train_labels, test_labels}
+dataset = SimpleNamespace(train_images=train_images, test_images=test_images, train_labels=train_labels, test_labels=test_labels)
