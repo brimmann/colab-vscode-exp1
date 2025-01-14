7840e4cde17def5a008ad0c6cc3f50fd05aca422
Rashid <rashidstanikzai.net@gmail.com>
2025-01-14 17:10:58 +0500

    nothing

--- models.py
+++ models.py
@@ -1,11 +1,21 @@
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Flatten, Dense
 
-inputs = Input(shape=(28, 28), name='input_layer')
-x = Flatten(name='flatten_layer')(inputs)
-x = Dense(128, activation='relu', name='hidden_dense')(x)
-outputs = Dense(10, activation='softmax', name='output_layer')(x)
-model1 = Model(inputs=inputs, outputs=outputs, name='functional_model')
+
+
+def get_model1():
+    inputs = Input(shape=(28, 28), name='input_layer')
+    x = Flatten(name='flatten_layer')(inputs)
+    x = Dense(128, activation='relu', name='hidden_dense')(x)
+    outputs = Dense(10, activation='softmax', name='output_layer')(x)
+    return Model(inputs=inputs, outputs=outputs, name='functional_model')
+
+def get_model2():
+    inputs = Input(shape=(28, 28), name='input_layer')
+    x = Flatten(name='flatten_layer')(inputs)
+    x = Dense(128, activation='relu', name='hidden_dense')(x)
+    outputs = Dense(10, activation='softmax', name='output_layer')(x)
+    return Model(inputs=inputs, outputs=outputs, name='functional_model')
 
 def compile_model(model):
     model.compile(optimizer='adam',
