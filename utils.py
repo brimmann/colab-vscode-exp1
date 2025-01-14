6b4cff8b2e0529d6ebf849e80c8d7d25b32ae240
Rashid <rashidstanikzai.net@gmail.com>
2025-01-14 16:42:27 +0500

    nothing

--- utils.py
+++ utils.py
@@ -6,3 +6,11 @@ def compile_model():
     model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
+
+def say_model():
+    model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+def say_hello2(name):
+    print("Hello from utils.py!", name)              
